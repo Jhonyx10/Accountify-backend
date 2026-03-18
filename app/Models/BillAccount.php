e229538bc5ef0ed14ab70e9b31b382910c1898a5
Jhonyx10 <occ.ordaniza.jhonmark10@gmail.com>
2026-03-18 09:00:30 +0800

    feat: implement global company scope and BelongsToCompany trait

--- app/Models/BillAccount.php
+++ app/Models/BillAccount.php
@@ -3,8 +3,10 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Traits\BelongsToCompany;
 
 class BillAccount extends Model
 {
+    use BelongsToCompany;
     //
 }
--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -5,10 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use App\Traits\BelongsToCompany;
 
 class Customer extends Authenticatable
 {
     use HasFactory;
+    use BelongsToCompany;
 
     protected $fillable = [
         'customer_id',
